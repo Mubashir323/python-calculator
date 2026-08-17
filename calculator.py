0009d431ac8dff490da8a3d8f64592747f52dbbc
Mubashir Chanzaib <mubashirabbxi0323@gmail.com>
2026-08-17 21:00:56 +0500

    Add first Python calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print("My Python Calculator")
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+result = number1 + number2
+
+print("The answer is:", result)
